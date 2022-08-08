29f7fb305695e34309fdcaf880dce8b1df7b0ac8
seanv999 <79374771+seanv999@users.noreply.github.com>
2022-08-08 02:07:13 +0000

    Update findTheButton.lua

--- findTheButton.lua
+++ findTheButton.lua
@@ -1,32 +1,25 @@
-canClick = true
 Players = game:GetService("Players")
 LocalPlayer = Players.LocalPlayer
 camera = workspace.CurrentCamera
 runservice = game:GetService("RunService")
+
 tracers = false
 Esp = false
+
 function initESP()
 
-	boxEsp = Drawing.new("Square")
-	boxEsp.Color = Color3.fromRGB(255,255,0)
+	--Button ESP
+	boxEsp = Drawing.new("Circle")
 	boxEsp.Visible = true
-	boxEsp.Size = Vector2.new(60,50)
+	boxEsp.Radius = 60
 
-	BUTTON = Drawing.new("Text")
-	BUTTON.Visible = true
-	BUTTON.Center = true
-	BUTTON.Outline = false
-	BUTTON.Font = 7
-	BUTTON.Color = Color3.fromRGB(0,0,0)
-	BUTTON.Size = 25
-	BUTTON.Text = "Button"
 	--tracers
-	local line = Drawing.new("Line")
+	line = Drawing.new("Line")
 	line.Visible = true
-	line.Color = Color3.fromRGB(255,255,255)
 	line.Thickness = 8
 	line.Transparency = 1
-
+	line.Color = Color3.fromRGB(255,255,255)
+	boxEsp.Color = Color3.fromRGB(255,255,255)
 
 	
 
@@ -35,7 +28,7 @@ function initESP()
 		
 		boxEsp.Visible = false
 		line.Visible = false
-		BUTTON.Visible = false
+		
 
 		currentMap = game:GetService("Players").localPlayer.leaderstats.Mode.value .. "Mode"
 		currentLevel = "Room".. game:GetService("Players").localPlayer.leaderstats.Room.value
@@ -44,7 +37,7 @@ function initESP()
 
 		if(Esp) then
 			boxEsp.Visible = true
-			BUTTON.Visible = true
+			
 		end
 
 		if(tracers) then
@@ -54,94 +47,107 @@ function initESP()
 
 		drop_pos = camera:WorldToViewportPoint(button.Position)
 		
-		BUTTON.Position = Vector2.new(drop_pos.X+30, drop_pos.Y+10)
+		
 		char = camera:WorldToViewportPoint(LocalPlayer.Character.HumanoidRootPart.Position)
 		line.From = Vector2.new(drop_pos.X,drop_pos.Y)
 		line.To = Vector2.new(char.X,char.Y)
-		boxEsp.Position = Vector2.new(drop_pos.X, drop_pos.Y)
+		boxEsp.Position = Vector2.new(drop_pos.X , drop_pos.Y)
+
 	end)
 end
 
 initESP()
 
+--GUI
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
 local Window = Library.CreateLib("Find The Button                     Seanv999 Deathskull93937", "BloodTheme")
+
 local Tab = Window:NewTab("Main")
+	local Section = Tab:NewSection("ESP")
+
+	Section:NewToggle("Button ESP", "Find the button!", function(state)
+		if state then
+			print("Toggle On")
+			Esp = true
+			print(Esp)
+		else
+			print("Toggle Off")
+			Esp = false
+		end
+		
+	end)
+
+	Section:NewToggle("Tracers", "Jump Infinitly", function(state)
+		if state then
+			print("Toggle On")
+			tracers = true
+		else
+			print("Toggle Off")
+			tracers = false
+			line:Destroy()
+		end
+		
+	end)
+
+	Section:NewButton("Teleport To Button", "Teleports Player To Button", function()
+		currentMap = game:GetService("Players").localPlayer.leaderstats.Mode.value .. "Mode"
+		--Map fixes
+		if currentMap == 'SpringMode' then
+			print('Fixed')
+			currentMap = 'SweetSpringMode'
+		end
+		currentLevel = "Room".. game:GetService("Players").localPlayer.leaderstats.Room.value
+		button = game:GetService("Workspace").Modes[currentMap][currentLevel]["Door/Button"].Button
+		game.Players.LocalPlayer.Character:MoveTo(button.Position)
+		
+	end)
+
+local Tab4 = Window:NewTab("ESP Settings")
+	local Section3 = Tab4:NewSection("Colors")
+
+	Section3:NewColorPicker("Button ESP Color", "Change color of button ESP", Color3.fromRGB(255,255,255), function(color)
+		print(color)
+	
+		boxEsp.Color = color
+	end)
+	
+	Section3:NewColorPicker("Tracer Color", "Change color of button ESP", Color3.fromRGB(255,255,255), function(color)
+		print(color)
+		line.Color = color
+	end)	
+
 local Tab2 = Window:NewTab("Misc")
+	local section2 = Tab2:NewSection("Basic")
+
+	section2:NewToggle("Infinite Jump", "Jump Infinitly", function(state)
+		if state then
+			print("Toggle On")
+			IJ = true
+		else
+			print("Toggle Off")
+			IJ = false
+		end
+		
+	end)
+
+	section2:NewSlider("Walk Speed", "Change walk speed", 500, 0, function(s)
+		LocalPlayer.Character.Humanoid.WalkSpeed = s
+	end)
+
+
+
+
 local Tab3 = Window:NewTab("Credit")
-local Section = Tab:NewSection("Cheats")
-local section2 = Tab2:NewSection("Basic")
-local creditSection = Tab3:NewSection("s3an999 On Instagram")
-local creditSection2 = Tab3:NewSection("kevinnnnndowdy On Instagram")
+	local creditSection = Tab3:NewSection("s3an999 On Instagram")
+	local creditSection2 = Tab3:NewSection("kevinnnnndowdy On Instagram")
 
 
-
-Section:NewButton("Teleport To Button", "Teleports Player To Button", function()
-    currentMap = game:GetService("Players").localPlayer.leaderstats.Mode.value .. "Mode"
-	--Map fixes
-	if currentMap == 'SpringMode' then
-		print('Fixed')
-		currentMap = 'SweetSpringMode'
-	end
-	currentLevel = "Room".. game:GetService("Players").localPlayer.leaderstats.Room.value
-	button = game:GetService("Workspace").Modes[currentMap][currentLevel]["Door/Button"].Button
-	game.Players.LocalPlayer.Character:MoveTo(button.Position)
-	
-end)
-
-section2:NewSlider("Walk Speed", "Change walk speed", 500, 0, function(s)
-    LocalPlayer.Character.Humanoid.WalkSpeed = s
-end)
-
-section2:NewToggle("Infinite Jump", "Jump Infinitly", function(state)
-    if state then
-        print("Toggle On")
-		IJ = true
-    else
-        print("Toggle Off")
-		IJ = false
-    end
-	
-end)
-
-
-Section:NewToggle("Box ESP", "Jump Infinitly", function(state)
-    if state then
-        print("Toggle On")
-		Esp = true
-		print(Esp)
-    else
-        print("Toggle Off")
-		Esp = false
-    end
-	
-end)
-
-Section:NewToggle("Tracers", "Jump Infinitly", function(state)
-    if state then
-        print("Toggle On")
-		tracers = true
-    else
-        print("Toggle Off")
-		tracers = false
-		line:Destroy()
-    end
-	
-end)
-
-
-
-
+--JUMP HAck
 game:GetService("UserInputService").JumpRequest:connect(function()
 	if IJ then
 		game:GetService"Players".LocalPlayer.Character:FindFirstChildOfClass'Humanoid':ChangeState("Jumping")
 	end
 end)
 
-game:GetService('RunService').Stepped:connect(function()
-	if noclip then
-	game.Players.LocalPlayer.Character.Humanoid:ChangeState(11)
-	end
-end)
 
 
